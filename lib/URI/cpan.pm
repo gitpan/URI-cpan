c3b2f194d663ef2e8b85a05bc68a02c091b7cb46
Ricardo SIGNES <rjbs@cpan.org>
2009-03-30 08:58:57 +0000

    Import of RJBS/URI-cpan-1.001 from CPAN.
    gitpan-cpan-distribution: URI-cpan
    gitpan-cpan-version:      1.001
    gitpan-cpan-path:         RJBS/URI-cpan-1.001.tar.gz
    gitpan-cpan-author:       RJBS
    gitpan-cpan-maturity:     released

--- lib/URI/cpan.pm
+++ lib/URI/cpan.pm
@@ -4,7 +4,7 @@ use warnings;
 package URI::cpan;
 use base qw(URI::_generic);
 
-our $VERSION = '1.000';
+our $VERSION = '1.001';
 
 =head1 NAME
 
@@ -35,7 +35,10 @@ Reserved for likely future use are:
 use Carp ();
 use URI::cpan::author;
 use URI::cpan::dist;
+use URI::cpan::distfile;
+use URI::cpan::module;
 use URI::cpan::package;
+use URI::cpan::dist;
 
 my %type_class = (
   author   => 'URI::cpan::author',
